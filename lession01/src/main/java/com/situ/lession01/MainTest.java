1a0d49895ec730524d1420c4d792ad9c1dd54acc
lishuo <1344793627@qq.com>
2020-12-07 19:34:16 +0800

    ssm 中mybatis学习

--- lession01/src/main/java/com/situ/lession01/MainTest.java
+++ lession01/src/main/java/com/situ/lession01/MainTest.java
@@ -10,11 +10,14 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.situ.lession01.dao.RoleDao;
 import com.situ.lession01.role.pojo.Role;
+import com.situ.lession01.service.RoleService;
+import com.situ.lession01.service.impl.RoleServiceImpl;
+import com.situ.lession01.util.MyBatisUtil;
 
 
 public class MainTest {
 
-	public static void main(String[] args) throws IOException {
+	public static void main1(String[] args) throws IOException {
 		String resource = "mybatis/mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
@@ -24,12 +27,27 @@ public class MainTest {
 		//通过session得到dao接口实例
 		RoleDao roleDao =  sqlSession.getMapper(RoleDao.class);
 		System.out.println("roleDao: "+roleDao);
-		roleDao.insert(new Role(1, "test"));
-		//执行提交
+		/* roleDao.insert(new Role(1, "test")); */
+		//一定要先获取到实例的所有内容
+		Role role = roleDao.findOne(1L);
+		System.out.println(role);
+		role.setRoleName("testUpdate");
+		role.setRoleType(0);
+		roleDao.update(role);
+		
+		roleDao.delete(6L);
+		
+		
+		System.out.println("roleDao.findAll()"+roleDao.findAll());
+		//执行提交 增删改都要提交 从而让事务完成
 		sqlSession.commit();
 		//数据库会话使用完毕后关闭
 		sqlSession.close();
 		
+	}
+	public static void main(String[] args) {
+		RoleDao roleDao = MyBatisUtil.getDao(RoleDao.class);
+		System.out.println(roleDao.findOneByName("testUpdate", 1L));
 	}
 
 }
--- lession01/src/main/java/com/situ/lession01/dao/RoleDao.java
+++ lession01/src/main/java/com/situ/lession01/dao/RoleDao.java
@@ -1,5 +1,9 @@
 package com.situ.lession01.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.situ.lession01.role.pojo.Role;
 /**
  * 持久化对象
@@ -7,5 +11,40 @@ import com.situ.lession01.role.pojo.Role;
  * @return
  */
 public interface RoleDao {
+	/**
+	 * 插入一条新数据
+	 * @param role
+	 * @return
+	 */
 	int insert(Role role);
+	/**
+	 * 查找一个实例
+	 * @param rowId
+	 * @return
+	 */
+	Role findOne(Long rowId);
+	/**
+	 * 查找所有的实例
+	 * @return
+	 */
+	List<Role> findAll();
+	/**
+	 * 修改
+	 * @param role
+	 * @return
+	 */
+	int update(Role role);
+	/**
+	 * 删除
+	 * @param rowId
+	 * @return
+	 */
+	int delete(Long rowId);
+	/**
+	 * 使用名字和id查找到一个实例
+	 * @param roleName
+	 * @param rowId
+	 * @return
+	 */
+	Role findOneByName(@Param("roleName")String roleName,@Param("rowId")Long rowId);
 }
